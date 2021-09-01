4e50d02487e58c4bffe4d24c3f29c392d054f336
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-09-01 14:21:22 -0700

    Adapt to the new base environment API

--- environments/silly/__init__.py
+++ environments/silly/__init__.py
@@ -1,28 +1,50 @@
 import numpy as np
-from badger import environment
+from badger import environment, get_env
 from badger.interface import Interface
 from operator import itemgetter
+import logging
+
 
 class Environment(environment.Environment):
 
     name = 'silly'
+    var_channel_map = {
+        'q1': 'c1',
+        'q2': 'c2',
+        'q3': 'c3',
+        'q4': 'c4',
+    }
 
     def __init__(self, interface: Interface, params):
+        # ENV, configs = get_env('TNK')
+        # env = ENV(interface, configs['params'])
+        # print(env)
         super().__init__(interface, params)
 
-    def get_x(self):
-        d = itemgetter('dimension')(self.params)
-        channels = [f'c{i + 1}' for i in range(d)]
-        x = np.array(self.interface.get_values(channels))
+    def get_var(self, var):
+        return self.interface.get_value(self.var_channel_map[var])
 
-        return x
+    def set_var(self, var, x):
+        self.interface.set_value(self.var_channel_map[var], x)
 
-    def set_x(self, x: np.ndarray):
-        d = itemgetter('dimension')(self.params)
-        channels = [f'c{i + 1}' for i in range(d)]
-        self.interface.set_values(channels, x.tolist())
-
-    def get_y(self):
-        y = np.array(self.interface.get_values(['norm']))
-
-        return y
+    def get_obs(self, obs):
+        if obs == 'l1':
+            values = self.interface.get_values(
+                ['c1', 'c2', 'c3', 'c4'])
+            return np.sum(np.abs(values))
+        elif obs == 'l2':
+            return self.interface.get_value('norm')
+        elif obs == 'min':
+            values = self.interface.get_values(
+                ['c1', 'c2', 'c3', 'c4'])
+            return np.min(values)
+        elif obs == 'max':
+            values = self.interface.get_values(
+                ['c1', 'c2', 'c3', 'c4'])
+            return np.max(values)
+        elif obs == 'mean':
+            values = self.interface.get_values(
+                ['c1', 'c2', 'c3', 'c4'])
+            return np.mean(values)
+        else:
+            logging.warn(f'Unsupported observation {obs}')
