d05c16d9087e10bc14e660cb4e63d95184ab6bf4
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-10-06 15:52:27 -0700

    Generalize the basic BO algo

--- algorithms/basic_bo/__init__.py
+++ algorithms/basic_bo/__init__.py
@@ -4,21 +4,21 @@ from bayes_opt import BayesianOptimization
 
 
 def optimize(evaluate, params):
-    random_state, init_points, n_iter = itemgetter(
-        'random_state', 'init_points', 'n_iter')(params)
+    dimension, random_state, init_points, n_iter = itemgetter(
+        'dimension', 'random_state', 'init_points', 'n_iter')(params)
 
-    def _evaluate(varx, vary, varz):
-        X = np.array([varx, vary, varz]).reshape(1, -1)
-        Y = evaluate(X)
+    def _evaluate(**kwargs):
+        var_list = [kwargs[f'v{i}'] for i in range(dimension)]
+        X = np.array(var_list).reshape(1, -1)
+        Y, _, _ = evaluate(X)
 
-        return Y
+        # BO assume a maximize problem
+        return -Y[0, 0]
 
     # bounds on input params
-    pbounds = {
-        'varx': (0.44, 0.55),
-        'vary': (-0.02, 0.02),
-        'varz': (-0.02, 0.02)
-    }
+    pbounds = {}
+    for i in range(dimension):
+        pbounds[f'v{i}'] = (0, 1)
 
     optimizer = BayesianOptimization(
         f=_evaluate,
