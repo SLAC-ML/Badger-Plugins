93c55e000cd8f036628f89d2c23b144a3abbcd2f
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-11-16 11:12:05 -0800

    Handle the case when expected value cannot reach

--- interfaces/epics/__init__.py
+++ interfaces/epics/__init__.py
@@ -1,8 +1,8 @@
+import time
 import numpy as np
-from badger import interface
 from operator import itemgetter
-import logging
 import epics
+from badger import interface
 
 epics.ca.DEFAULT_CONNECTION_TIMEOUT = 0.1
 
@@ -36,6 +36,8 @@ class Interface(interface.Interface):
             if value is not None:
                 break
 
+            time.sleep(0.1)
+
         return value
 
     def set_value(self, channel: str, value):
@@ -45,8 +47,23 @@ class Interface(interface.Interface):
             pv = epics.get_pv(channel)
             self.pvs[channel] = pv
 
-        if not pv.connected:
+        if not pv.wait_for_connection(1):
             # TODO: consider throwing an exception here
             return None
-        else:
-            return pv.put(value)
+
+        # Wait for no longer 5s
+        pv.put(value, wait=True, timeout=3)
+        count_down = 2  # second
+        while count_down > 0:
+            _value = pv.get()
+            if value:
+                if np.isclose(_value, value, rtol=1e-3):
+                    return _value
+            else:
+                if np.isclose(_value, value, atol=1e-3):
+                    return _value
+
+            time.sleep(0.1)
+            count_down -= 0.1
+
+        raise Exception(f'PV {channel} (current: {_value}) cannot reach expected value ({value})!')
