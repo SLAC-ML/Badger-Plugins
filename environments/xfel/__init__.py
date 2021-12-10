4ae3026f477ad1f05b583a8b56c3b991e14b840f
michael-boese <michael.boese@desy.de>
2021-12-10 09:53:22 +0100

    add new observation 'disp_target' to xfel env

--- environments/xfel/__init__.py
+++ environments/xfel/__init__.py
@@ -42,7 +42,7 @@ class Environment(environment.Environment):
     # TODO: add losses
     @staticmethod
     def list_obses():
-        return ['charge', 'sases', 'beam_energy', 'wavelength', 'ref_sase_signal', 'target_sase']
+        return ['charge', 'sases', 'beam_energy', 'wavelength', 'ref_sase_signal', 'target_sase', 'target_disp']
 
     @staticmethod
     def get_default_params():
@@ -142,6 +142,36 @@ class Environment(environment.Environment):
             orbit2 = np.zeros(len(bpms))  # just [0, 0, 0, ... ]
             target = np.sqrt(np.sum((orbit2 - orbit1) ** 2))
 
+            return target
+        elif obs == "target_disp":
+            bpms = ["XFEL.DIAG/BPM/BPMA.59.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.72.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.75.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.77.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.80.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.82.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.85.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.87.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.90.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.92.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMF.95.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMC.134.L1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.117.I1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMC.158.L1/X.ALL",
+                    "XFEL.DIAG/BPM/BPMA.179.B1/X.ALL"]
+            Vinit = self.interface.get_value("XFEL.RF/LLRF.CONTROLLER/CTRL.A1.I1/SP.AMPL")
+            orbit1 = self.read_bpms(bpms=bpms, nreadings=7)
+
+            time.sleep(0.1)
+            self.interface.set_value("XFEL.RF/LLRF.CONTROLLER/CTRL.A1.I1/SP.AMPL", Vinit - 2)
+            time.sleep(0.9)
+
+            orbit2 = self.read_bpms(bpms=bpms, nreadings=7)
+
+            self.interface.set_value("XFEL.RF/LLRF.CONTROLLER/CTRL.A1.I1/SP.AMPL", Vinit)
+            time.sleep(0.9)
+
+            target = -np.sqrt(np.sum((orbit2 - orbit1)**2))
             return target
 
     def read_bpms(self, bpms, nreadings):
