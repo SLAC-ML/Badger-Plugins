acd304c9e21d6be4225dda97f2a32e9781da1468
Zhe Zhang <zhezhang@slac.stanford.edu>
2022-06-29 14:17:58 -0700

    Support constraints in the xopt extension

--- extensions/xopt/utils.py
+++ extensions/xopt/utils.py
@@ -9,16 +9,19 @@ def convert_evaluate(evaluate, configs):
     obj_names = [next(iter(d)) for d in configs['objectives']]
     rules = [d[next(iter(d))] for d in configs['objectives']]
     try:
-        print(configs['constraints'])
+        configs_cons = configs['constraints'] or []
     except:
-        print('no constraints')
+        configs_cons = []
+    con_names = [next(iter(d)) for d in configs_cons]
+    relations = [d[next(iter(d))] for d in configs_cons]
 
     def _evaluate(inputs):
         x = np.array([inputs[var_name] for var_name in var_names])
         X = norm(x, vranges[:, 0], vranges[:, 1]).reshape(1, -1)
-        Y, _, _, _ = evaluate(X)
+        Y, I, E, _ = evaluate(X)
 
         outputs = {}
+        # Add objectives
         for i, obj_name in enumerate(obj_names):
             rule = rules[i]
             obs = Y[0, i]
@@ -26,6 +29,20 @@ def convert_evaluate(evaluate, configs):
                 outputs[obj_name] = -obs
             else:
                 outputs[obj_name] = obs
+        # Add constraints
+        count_i = 0
+        count_e = 0
+        for i, con_name in enumerate(con_names):
+            relation, thres = relations[i][:2]
+            if relation == 'GREATER_THAN':
+                outputs[con_name] = I[0, count_i] + thres
+                count_i += 1
+            elif relation == 'LESS_THAN':
+                outputs[con_name] = thres - I[0, count_i]
+                count_i += 1
+            else:
+                outputs[con_name] = E[0, count_e] + thres
+                count_e += 1
 
         return outputs
 
