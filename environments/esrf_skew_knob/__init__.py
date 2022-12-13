7eeb034a31b6fbd9d9336f2ce479a147308e7385
michael-boese <michael.boese@desy.de>
2022-12-13 13:53:08 +0100

    add env for esrf

---
+++ environments/esrf_skew_knob/__init__.py
@@ -0,0 +1,83 @@
+import time
+import numpy as np
+from badger import environment
+from badger.interface import Interface
+import pathlib
+
+
+
+class Knobs():
+    def __init__(self, csv_file_name):
+        self._matrix = self.load_skrew_knob_from_csv(csv_file_name)
+        # there are no names in csv so generate names
+        self._row_names = [f"knob-{i}" for i in range(self.get_count())]
+
+    @staticmethod
+    def load_skrew_knob_from_csv(filename) -> np.ndarray:
+        return np.genfromtxt(filename, delimiter=',')
+
+    def get_count(self):
+        return self._matrix.shape[0]
+
+    def get_names(self):
+        return self._row_names
+
+    def gen_matrix(self, vars):
+        if len(vars) == len(self._row_names):
+            return self._matrix
+        vars_hash = set(vars)
+        remove_row_idx = [idx for idx, name in enumerate(self._row_names) if not name in vars_hash]
+        return np.delete(self._matrix, remove_row_idx, axis=0) # remove rows
+
+
+
+class Environment(environment.Environment):
+    path = pathlib.Path(__file__).parent.resolve()
+    knobs = Knobs(path / "data" / "SkewKnobSF2A.csv")
+    name = 'esrf_skew_knob'
+
+    def __init__(self, interface: Interface, params):
+        self.limits_knobs = { name : [-1000, 1000] for name in Environment.knobs.get_names()}
+        super().__init__(interface, params)
+
+    def _get_vrange(self, var):
+        return self.limits_knobs[var]
+
+    @staticmethod
+    def list_vars():
+        return Environment.knobs.get_names()
+    
+    # TODO: add losses
+    @staticmethod
+    def list_obses():
+        return ['emittance']
+
+    @staticmethod
+    def get_default_params():
+        return {
+            'waiting_time': 1,
+        }
+
+    def _get_var(self, var):
+        # TODO: update pv limits every time?
+        return self.interface.get_value(var)
+
+    def _set_var(self, var, x):
+        raise Exception("values have to be set at once!")
+
+    def _set_vars(self, vars, _x):
+        print(f"value names {vars}")
+        skews = np.sum(_x * np.transpose(Environment.knobs.gen_matrix(vars)))
+        self.interface.set_value('srmag/sqp/all', 'CorrectionStrengths', skews) 
+
+    def _get_obs(self, obs):
+        try:
+            dt = self.params['waiting_time']
+        except KeyError:
+            dt = 0
+        time.sleep(dt)
+
+
+        if obs == 'emittance':
+            return self.interface.get_value('srdiag/emittance/id07/Emittance_V')
+
---
+++ interfaces/tango/__init__.py
@@ -0,0 +1,22 @@
+import tango
+from badger import interface
+
+
+class Interface(interface.Interface):
+
+    name = 'tango'
+
+    def __init__(self, params=None):
+        super().__init__(params)
+
+    @staticmethod
+    def get_default_params():
+        return None
+
+    def get_value(self, channel: str):
+        attr = tango.AttributeProxy(channel)
+        return attr.read().value
+
+    def set_value(self, channel: str, value, attr: str):
+        dev = tango.DeviceProxy(channel)
+        dev.write_attribute(attr, value)
