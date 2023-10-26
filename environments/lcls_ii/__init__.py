bb217e7611b8afe042640510239722dd2131f409
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-10-25 19:59:15 -0700

    Add pulse intensity acquisition logic
    And the basic fault check logic

--- environments/lcls_ii/__init__.py
+++ environments/lcls_ii/__init__.py
@@ -1,7 +1,9 @@
+import logging
 import time
 import numpy as np
+from edef import EventDefinition
 from badger import environment
-from badger.errors import BadgerNoInterfaceError
+from badger.errors import BadgerNoInterfaceError, BadgerEnvObsError
 
 
 class Environment(environment.Environment):
@@ -26,16 +28,26 @@ class Environment(environment.Environment):
         'QUAD:LTUS:680:BCTRL': [],
     }
     observables = [
-        'EM1K0:GMD:HPS:milliJoulesPerPulse',
-        'EM2K0:XGMD:HPS:milliJoulesPerPulse',
-        'EM1K0:GMD:HPS:AvgPulseIntensity',
-        'EM2K0:XGMD:HPS:AvgPulseIntensity',
+        'sxr_pulse_intensity',
     ]
 
     # Env params
+    method: int = 1
+    # For method 0
+    xgmd: bool = False
+    avg: bool = False
+    # For method 1
+    event_code: str = 'SCS'  # could also be '31', '32'
+    custom_acq_rate: float = 100  # for custom event code
+    points: int = 100
+    loss_pv: str = 'LBLM:COL0:862:A:I0_LOSSSCSHH'  # loss monitor PV
+    stats: str = 'percent_80'
+    # Var setters
     use_check_var: bool = False  # if check var reaches the target value
-    trim_delay: float = 3.0  # in second
-    # fault_timeout: float = 5.0  # in second
+    check_var_timeout: float = 3.0  # tumeout for the var check
+    trim_delay: float = 7.0  # in second
+    # MPS fault check
+    check_fault_timeout: float = 5.0  # in second
 
     def get_bounds(self, variable_names):
         if self.interface is None:
@@ -90,7 +102,6 @@ class Environment(environment.Environment):
                                 if v.endswith(':BCTRL')]
         variable_status = self.interface.get_values(variable_ready_flags)
 
-        timeout = 3  # second
         time_start = time.time()
         while np.any(np.array(variable_status.values())):
             time.sleep(0.1 * np.random.rand())
@@ -98,5 +109,113 @@ class Environment(environment.Environment):
             variable_status = self.interface.get_values(variable_ready_flags)
 
             time_elapsed = time.time() - time_start
-            if time_elapsed > timeout:
+            if time_elapsed > self.check_var_timeout:
                 break
+
+        if self.trim_delay:
+            time.sleep(self.trim_delay)  # extra time for stablizing orbits
+
+    def get_sxr_pulse_intensity(self):
+        # self.method
+        # 0: scalar
+        # 1: BSA buffer
+        # 2: manually data collection
+        if self.method == 0:
+            if self.xgmd:
+                if self.avg:
+                    value = self.interface.get_value(
+                        'EM2K0:XGMD:HPS:AvgPulseIntensity')
+                else:
+                    value = self.interface.get_value(
+                        'EM2K0:XGMD:HPS:milliJoulesPerPulse')
+            else:
+                if self.avg:
+                    value = self.interface.get_value(
+                        'EM1K0:GMD:HPS:AvgPulseIntensity')
+                else:
+                    value = self.interface.get_value(
+                        'EM1K0:GMD:HPS:milliJoulesPerPulse')
+
+            return value
+        elif self.method == 1:
+            points = self.points
+            logging.info(f'Get value of {points} points')
+
+            req_rate = self.interface.get_value('TPG:SYS0:1:DST04:REQRATE')
+
+            if not req_rate:
+                raise BadgerEnvObsError
+
+            req_rate = float(req_rate)
+            if self.event_code == 'SCS':
+                if req_rate < 100:
+                    PV_gas = 'EM1K0:GMD:HPS:milliJoulesPerPulseHSTSCSTH'
+                    rate = 10
+                else:
+                    PV_gas = 'EM1K0:GMD:HPS:milliJoulesPerPulseHSTSCSHH'
+                    rate = 100
+            else:
+                PV_gas = f'EM1K0:GMD:HPS:milliJoulesPerPulseHST{self.event_code}'
+                rate = self.custom_acq_rate
+            logging.info(f'SXR dump acquisition rate: {rate} Hz')
+
+            # Wait enough time to accumulate sufficient data points in buffers
+            nap_time = points / rate
+            time.sleep(nap_time)
+
+            data_raw = self.interface.get_value(PV_gas)
+            data = data_raw[-points:]
+            obj_tar = np.percentile(data, 80)
+            obj_mean = np.mean(data)
+            obj_median = np.median(data)
+            obj_std = np.std(data)
+
+            stats_dict = {
+                'percent_80': obj_tar,
+                'mean': obj_mean,
+                'median': obj_median,
+                'std': obj_std,
+            }
+
+            return stats_dict[self.stats]
+        elif self.method == 2:
+            raise NotImplementedError()
+        else:
+            raise NotImplementedError()
+
+    def check_fault_status(self):
+        ts_start = time.time()
+        while True:
+            req_rate = self.interface.get_value('TPG:SYS0:1:DST04:REQRATE')
+            act_rate = self.interface.get_value('TPG:SYS0:1:DST04:RATE')
+            is_rate_matched = (req_rate == act_rate)
+
+            permit_MPS = self.interface.get_value(
+                'SIOC:SYS0:MP00:SC_SXR_BC', as_string=True)
+            is_beam_on = (permit_MPS != 'Beam Off')
+
+            if is_rate_matched and is_beam_on:
+                break
+            else:
+                ts_curr = time.time()
+                dt = ts_curr - ts_start
+                if dt > self.check_fault_timeout:
+                    raise BadgerEnvObsError
+
+                time.sleep(0.1 * np.random.rand())
+
+    def get_observables(self, observable_names: list[str]) -> dict:
+        if self.interface is None:
+            raise BadgerNoInterfaceError
+
+        # Make sure machine is not in a fault state
+        self.check_fault_status()
+
+        observable_outputs = {}
+        for obs in observable_names:
+            if obs == 'sxr_pulse_intensity':
+                value = self.get_sxr_pulse_intensity()
+
+            observable_outputs[obs] = value
+
+        return observable_outputs
