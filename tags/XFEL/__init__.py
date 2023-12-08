60180ff71894ddfe82004bf51a84b430f4d3f432
Jannik Woehnert <jannik.woehnert@desy.de>
2023-12-08 15:30:35 +0100

    extension to 12_feature/machine_dependent_filters of Badger

---
+++ tags/XFEL/__init__.py
@@ -0,0 +1,6 @@
+
+machine_tags = {'machine': 'XFEL',
+                'region': ['SA1', 'SA2', 'SA3'],
+                'objective': ['launch', 'correctors', 'quads'],
+                'gain': ['setup', 'tuning']
+                }
